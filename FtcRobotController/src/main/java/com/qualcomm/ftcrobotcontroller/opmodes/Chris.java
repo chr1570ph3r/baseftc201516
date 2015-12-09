9c312465187669c4b82fb025c34b4b33ee488aea
chr1570ph3r <christopher.t@bronxsoftware.org>
2015-12-09 15:50:36 -0500

    Update and rename Chris to Chris.java

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Chris.java
@@ -0,0 +1,6 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
